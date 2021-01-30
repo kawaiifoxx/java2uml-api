d53a4d6c9e89f78bcf26b9a83139ef3aee1df0cc
kawaiifoxx <shreyanshg06@gmail.com>
2021-01-30 18:10:18 +0530

    Initial commit, spring boot project setup.

---
+++ src/main/java/org/java2uml/java2umlapi/Java2umlApiApplication.java
@@ -0,0 +1,13 @@
+package org.java2uml.java2umlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2umlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java2umlApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/java2uml/java2umlapi/Java2umlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java2uml.java2umlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2umlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
