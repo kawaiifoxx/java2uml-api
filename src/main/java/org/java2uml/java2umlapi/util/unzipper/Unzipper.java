b4f80b9a1280b8a2b5a392dea0811b8b949bcd65
kawaiifoxx <shreyanshg06@gmail.com>
2021-02-09 15:30:36 +0530

    Added a new util class Unzipper and unit tests for corresponding class.

---
+++ src/main/java/org/java2uml/java2umlapi/util/unzipper/Unzipper.java
@@ -0,0 +1,89 @@
+package org.java2uml.java2umlapi.util.unzipper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * <p>
+ *     Utility class to unzip files from a given folder to a given folder.
+ * </p>
+ *
+ * @author kawaiifox
+ */
+public class Unzipper {
+
+    /**
+     * unzip a given file from srcZipPath and generate unzipped files in UnzipOutput directory in destDirPath.
+     *
+     * @param srcZipPath path of source zip file.
+     * @param destDirPath path of destination where output needs to be generated.
+     * @return path of "UnzipOutput" directory.
+     * @throws IOException if unable to create directory then throws IOException.
+     */
+    public static File unzipDir(Path srcZipPath, Path destDirPath) throws IOException {
+        File zipFile = new File(srcZipPath.toAbsolutePath().toString());
+        File destDir = new File(destDirPath.toAbsolutePath().toString() + File.separator + "UnzipOutput");
+
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+        ZipEntry zipEntry = zis.getNextEntry();
+
+        while (zipEntry != null) {
+            File newFile = newFile(destDir, zipEntry);
+
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory " + newFile);
+                }
+            } else {
+                File parent = newFile.getParentFile();
+
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory " + parent);
+                }
+
+                FileOutputStream fos = new FileOutputStream(newFile);
+
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+
+                fos.close();
+            }
+
+            zipEntry = zis.getNextEntry();
+        }
+
+        zis.closeEntry();
+        zis.close();
+
+        return destDir;
+    }
+
+    /**
+     * Generates new file in given destination directory according to zipEntry.
+     *
+     * @param destDir Directory where file needs to be created.
+     * @param zipEntry ZipEntry for file being created.
+     * @return Returns newly created file.
+     * @throws IOException exception is thrown in case of zip slip.
+     */
+    private static File newFile(File destDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destDir, zipEntry.getName());
+
+        String destDirPath = destDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
+}
---
+++ src/test/java/org/java2uml/java2umlapi/util/unzipper/UnzipperTest.java
@@ -0,0 +1,50 @@
+package org.java2uml.java2umlapi.util.unzipper;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class UnzipperTest {
+
+    private static final String SRC = "src/test/testSources/JavaParserFacadeTests/testParserClass/ProjectTest/thymeleaf-demo-thymeleaf-demo.zip";
+    private static final String DST = "src/test/testOutput";
+
+    @Test
+    @DisplayName("When using Unzipper, unzips files from SRC path and generate output in DST path.")
+    void testUnzipDir()  throws IOException {
+        File destDir = Unzipper.unzipDir(Path.of(SRC), Path.of(DST));
+
+        if (Files.notExists(destDir.toPath())) {
+            fail("Unzipped Directory not found.");
+        }
+
+        //clean up.
+        deleteDirectory(destDir.toPath());
+    }
+
+    private void deleteDirectory(Path path) {
+        if (Files.isDirectory(path)) {
+            File dir = new File(path.toAbsolutePath().toString());
+            var files = dir.listFiles();
+            Arrays.stream(files).forEach(file -> deleteDirectory(Path.of(file.getAbsolutePath())));
+
+            if (!dir.delete()) {
+                throw new RuntimeException("Failed to delete directory: "+ dir);
+            }
+
+            return;
+        }
+
+        if (!path.toFile().delete()) {
+            throw new RuntimeException("Failed to delete file: "+ path);
+        }
+
+    }
+}
