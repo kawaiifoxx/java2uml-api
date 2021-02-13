8a9857f92b5316f5bad239e2fdcab040bfaf75a3
kawaiifoxx <shreyanshg06@gmail.com>
2021-02-13 16:08:03 +0530

    Added tests for ParsedExternalComponent in ParsedExternalComponentTest.

---
+++ src/test/java/org/java2uml/java2umlapi/umlComponenets/ParsedExternalComponentTest.java
@@ -0,0 +1,53 @@
+package org.java2uml.java2umlapi.umlComponenets;
+
+import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedTypeDeclaration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("When using ParsedExternalComponent,")
+class ParsedExternalComponentTest {
+
+    ParsedExternalComponent parsedComponent;
+
+    private final String fullyQualifiedName = "co.test.Test";
+    private final boolean isClass = true;
+    private final boolean isInterface = false;
+
+    @BeforeEach
+    void setUp() {
+        var resolvedReferenceType = mock(ResolvedReferenceTypeDeclaration.class);
+        lenient().doReturn(false).when(resolvedReferenceType).isGeneric();
+
+        var resolvedDeclaration = mock(ResolvedTypeDeclaration.class);
+        doReturn(fullyQualifiedName).when(resolvedDeclaration).getQualifiedName();
+        lenient().doReturn(isClass).when(resolvedDeclaration).isClass();
+        lenient().doReturn(isInterface).when(resolvedDeclaration).isInterface();
+        lenient().doReturn(resolvedReferenceType).when(resolvedDeclaration).asReferenceType();
+        parsedComponent = new ParsedExternalComponent(resolvedDeclaration);
+    }
+
+    @Test
+    @DisplayName("using getName, should return name of external component.")
+    void testGetName() {
+        assertEquals(fullyQualifiedName, parsedComponent.getName(), "returned name is incorrect.");
+    }
+
+    @Test
+    @DisplayName("using toUML, should return correct uml syntax for ParsedExternalComponent.")
+    void testToUML() {
+        var uml = parsedComponent.toUML();
+
+        assertTrue(uml.contains(fullyQualifiedName));
+        assertEquals(isClass, uml.contains("class"));
+        assertEquals(isInterface, uml.contains("interface"));
+    }
+}
