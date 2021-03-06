28ce32555197fa7a1d17d9cd64c09cf6e8512b6a
kawaiifoxx <shreyanshg06@gmail.com>
2021-03-06 18:40:47 +0530

    Added logs to .gitignore.

---
+++ src/test/java/org/java2uml/java2umlapi/visitors/LightWeightExtractorTest.java
@@ -0,0 +1,15 @@
+package org.java2uml.java2umlapi.visitors;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class LightWeightExtractorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
