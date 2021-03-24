6a584e65c6d1cf29715e1c5b2a210fbf7cd1a1ad
kawaiifoxx <shreyanshg06@gmail.com>
2021-03-24 16:26:37 +0530

    Added an exception called MethodNameToMethodIdNotFoundException

---
+++ src/main/java/org/java2uml/java2umlapi/restControllers/exceptions/MethodNameToMethodIdNotFoundException.java
@@ -0,0 +1,44 @@
+package org.java2uml.java2umlapi.restControllers.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * <p>
+ * This exception can be thrown if you are unable to fetch map of method names and there ids.
+ * </p>
+ *
+ * @author kawaiifox
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class MethodNameToMethodIdNotFoundException extends RuntimeException {
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public MethodNameToMethodIdNotFoundException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new runtime exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * {@code cause} is <i>not</i> automatically incorporated in
+     * this runtime exception's detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A {@code null} value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     * @since 1.4
+     */
+    public MethodNameToMethodIdNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
