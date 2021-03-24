b0e573cdfae8d9812f1f67ac9a4a94676e21af7a
kawaiifoxx <shreyanshg06@gmail.com>
2021-03-24 13:03:35 +0530

    Added two annotation in AppConfig @EnableConfigurationProperties for enabling custom properties in application.properties file and @ConfigurationPropertiesScan for enabling scanning of properties in the java2uml project.

--- src/main/java/org/java2uml/java2umlapi/AppConfig.java
+++ src/main/java/org/java2uml/java2umlapi/AppConfig.java
@@ -1,9 +1,13 @@
 package org.java2uml.java2umlapi;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class AppConfig {
 }
