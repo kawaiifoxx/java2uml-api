907f9250cc3ef0c35ce55767edc0a2f9c7845364
kawaiifoxx <shreyanshg06@gmail.com>
2021-03-24 15:46:16 +0530

    Added a Spring HATEOAS assembler for UMLBody entity.

---
+++ src/main/java/org/java2uml/java2umlapi/modelAssemblers/UMLBodyAssembler.java
@@ -0,0 +1,36 @@
+package org.java2uml.java2umlapi.modelAssemblers;
+
+import org.java2uml.java2umlapi.fileStorage.entity.ProjectInfo;
+import org.java2uml.java2umlapi.lightWeight.UMLBody;
+import org.java2uml.java2umlapi.restControllers.ProjectInfoController;
+import org.java2uml.java2umlapi.restControllers.UMLController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+/**
+ * <p>
+ * Maps UMLBody entity to EntityModel for representational purposes.<br>
+ * Adds few useful links to entityModel generated from this class.
+ * </p>
+ *
+ * @author kawaiifox
+ */
+@SuppressWarnings("NullableProblems")
+@Component
+public class UMLBodyAssembler implements RepresentationModelAssembler<UMLBody, EntityModel<UMLBody>> {
+
+    @Override
+    public EntityModel<UMLBody> toModel(UMLBody entity) {
+        ProjectInfo projectInfo = entity.getProjectInfo();
+        return EntityModel.of(
+                entity,
+                linkTo(methodOn(UMLController.class).getPUMLCode(projectInfo.getId())).withSelfRel(),
+                linkTo(methodOn(UMLController.class).getSvg(projectInfo.getId())).withRel("uml:svg"),
+                linkTo(methodOn(ProjectInfoController.class).one(projectInfo.getId())).withRel("projectInfo")
+        );
+    }
+}
