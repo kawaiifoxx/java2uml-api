66a13b3430058e921eaee3923c3af6ff6d4628ea
kawaiifoxx <shreyanshg06@gmail.com>
2021-03-24 12:03:11 +0530

    Added a new exception class FileStorageException.

---
+++ src/main/java/org/java2uml/java2umlapi/fileStorage/exceptions/FileStorageException.java
@@ -0,0 +1,40 @@
+package org.java2uml.java2umlapi.fileStorage.exceptions;
+
+/**
+ * <p>
+ * This exception can be thrown when some exception is encountered while storing or retrieving files.
+ * </p>
+ *
+ * @author kawaiifox.
+ */
+public class FileStorageException extends RuntimeException {
+    /**
+     * Constructs a new runtime exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * {@code cause} is <i>not</i> automatically incorporated in
+     * this runtime exception's detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A {@code null} value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     * @since 1.4
+     */
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public FileStorageException(String message) {
+        super(message);
+    }
+}
