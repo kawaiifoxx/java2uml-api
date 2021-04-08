db69d2c6998397900b0e81ceabde405b5ddfba15
Restyled.io <commits@restyled.io>
2021-04-08 11:21:00 +0000

    Restyled by clang-format

--- src/main/java/org/java2uml/java2umlapi/util/unzipper/Unzipper.java
+++ src/main/java/org/java2uml/java2umlapi/util/unzipper/Unzipper.java
@@ -17,83 +17,89 @@ import java.util.zip.ZipInputStream;
  */
 public abstract class Unzipper {
 
-    /**
-     * unzip a given file from srcZipPath and generate unzipped files in destDirPath.
-     *
-     * @param srcZipPath  {@link Path} of source zip file.
-     * @param destDirPath {@link Path} of destination where output needs to be generated.
-     * @return {@link File} directory containing unzipped files.
-     * @throws IOException if unable to create directory.
-     */
-    public static File unzipDir(Path srcZipPath, Path destDirPath) throws IOException {
-        File zipFile = new File(srcZipPath.toAbsolutePath().toString());
-        File destDir = new File(destDirPath.toAbsolutePath().toString());
-        ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
-        ZipEntry zipEntry = zis.getNextEntry();
+  /**
+   * unzip a given file from srcZipPath and generate unzipped files in
+   * destDirPath.
+   *
+   * @param srcZipPath  {@link Path} of source zip file.
+   * @param destDirPath {@link Path} of destination where output needs to be
+   *     generated.
+   * @return {@link File} directory containing unzipped files.
+   * @throws IOException if unable to create directory.
+   */
+  public static File unzipDir(Path srcZipPath, Path destDirPath)
+      throws IOException {
+    File zipFile = new File(srcZipPath.toAbsolutePath().toString());
+    File destDir = new File(destDirPath.toAbsolutePath().toString());
+    ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+    ZipEntry zipEntry = zis.getNextEntry();
 
-        while (zipEntry != null) {
-            File newFile = newFile(destDir, zipEntry);
+    while (zipEntry != null) {
+      File newFile = newFile(destDir, zipEntry);
 
-            if (zipEntry.isDirectory()) {
-                if (!newFile.isDirectory() && !newFile.mkdirs()) {
-                    throw new IOException("Failed to create directory " + newFile);
-                }
-            } else {
-                writeData(zis, newFile);
-            }
-
-            zipEntry = zis.getNextEntry();
+      if (zipEntry.isDirectory()) {
+        if (!newFile.isDirectory() && !newFile.mkdirs()) {
+          throw new IOException("Failed to create directory " + newFile);
         }
+      } else {
+        writeData(zis, newFile);
+      }
 
-        zis.closeEntry();
-        zis.close();
-        return destDir;
+      zipEntry = zis.getNextEntry();
     }
 
-    /**
-     * Writes the data from {@link ZipInputStream} in to the specified {@link File} newFile until
-     * the end of stream is reached.
-     *
-     * @param zis     {@link ZipInputStream} from which the data will be read.
-     * @param newFile {@link File} in which data will be written.
-     * @throws IOException if unable to create directory.
-     */
-    private static void writeData(ZipInputStream zis, File newFile) throws IOException {
-        byte[] buffer = new byte[1024];
-        File parent = newFile.getParentFile();
+    zis.closeEntry();
+    zis.close();
+    return destDir;
+  }
 
-        if (!parent.isDirectory() && !parent.mkdirs()) {
-            throw new IOException("Failed to create directory " + parent);
-        }
+  /**
+   * Writes the data from {@link ZipInputStream} in to the specified {@link
+   * File} newFile until the end of stream is reached.
+   *
+   * @param zis     {@link ZipInputStream} from which the data will be read.
+   * @param newFile {@link File} in which data will be written.
+   * @throws IOException if unable to create directory.
+   */
+  private static void writeData(ZipInputStream zis, File newFile)
+      throws IOException {
+    byte[] buffer = new byte[1024];
+    File parent = newFile.getParentFile();
 
-        FileOutputStream fos = new FileOutputStream(newFile);
-
-        int len;
-        while ((len = zis.read(buffer)) > 0) {
-            fos.write(buffer, 0, len);
-        }
-
-        fos.close();
+    if (!parent.isDirectory() && !parent.mkdirs()) {
+      throw new IOException("Failed to create directory " + parent);
     }
 
-    /**
-     * Generates new file in given destination directory according to zipEntry.
-     *
-     * @param destDir  Directory where file needs to be created.
-     * @param zipEntry ZipEntry for file being created.
-     * @return Returns newly created file.
-     * @throws IOException exception is thrown in case of zip slip.
-     */
-    private static File newFile(File destDir, ZipEntry zipEntry) throws IOException {
-        File destFile = new File(destDir, zipEntry.getName());
+    FileOutputStream fos = new FileOutputStream(newFile);
 
-        String destDirPath = destDir.getCanonicalPath();
-        String destFilePath = destFile.getCanonicalPath();
-
-        if (!destFilePath.startsWith(destDirPath + File.separator)) {
-            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
-        }
-
-        return destFile;
+    int len;
+    while ((len = zis.read(buffer)) > 0) {
+      fos.write(buffer, 0, len);
     }
+
+    fos.close();
+  }
+
+  /**
+   * Generates new file in given destination directory according to zipEntry.
+   *
+   * @param destDir  Directory where file needs to be created.
+   * @param zipEntry ZipEntry for file being created.
+   * @return Returns newly created file.
+   * @throws IOException exception is thrown in case of zip slip.
+   */
+  private static File newFile(File destDir, ZipEntry zipEntry)
+      throws IOException {
+    File destFile = new File(destDir, zipEntry.getName());
+
+    String destDirPath = destDir.getCanonicalPath();
+    String destFilePath = destFile.getCanonicalPath();
+
+    if (!destFilePath.startsWith(destDirPath + File.separator)) {
+      throw new IOException("Entry is outside of the target dir: " +
+                            zipEntry.getName());
+    }
+
+    return destFile;
+  }
 }
