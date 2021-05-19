cc9b7e67f3c9e20faf84aa2b0a77b4769805483c
kawaiifoxx <shreyanshg06@gmail.com>
2021-05-19 16:03:31 +0530

    Made Parser Abstract.

--- src/main/java/org/java2uml/java2umlapi/parser/Parser.java
+++ src/main/java/org/java2uml/java2umlapi/parser/Parser.java
@@ -29,7 +29,7 @@ import java.util.List;
  *
  * @author kawaiifox.
  */
-public class Parser {
+public abstract class Parser {
     /**
      * <p>
      * resolves all reference types in given java source directory and returns a SourceComponent.
