8604ed5cf2a7c5f7a755eabd057b1bb25df00ee5
kawaiifoxx <shreyanshg06@gmail.com>
2021-05-27 13:34:58 +0530

    Added swagger ui metadata to ProjectInfoController

--- src/main/java/org/java2uml/java2umlapi/restControllers/ProjectInfoController.java
+++ src/main/java/org/java2uml/java2umlapi/restControllers/ProjectInfoController.java
@@ -1,15 +1,24 @@
 package org.java2uml.java2umlapi.restControllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.java2uml.java2umlapi.fileStorage.entity.ProjectInfo;
 import org.java2uml.java2umlapi.fileStorage.repository.ProjectInfoRepository;
 import org.java2uml.java2umlapi.fileStorage.service.UnzippedFileStorageService;
 import org.java2uml.java2umlapi.lightWeight.service.MethodSignatureToMethodIdMapService;
 import org.java2uml.java2umlapi.modelAssemblers.ProjectInfoAssembler;
 import org.java2uml.java2umlapi.parsedComponent.service.SourceComponentService;
+import org.java2uml.java2umlapi.restControllers.error.ErrorResponse;
 import org.java2uml.java2umlapi.restControllers.exceptions.ProjectInfoNotFoundException;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import static org.java2uml.java2umlapi.restControllers.SwaggerDescription.*;
 
 /**
  * <p>
@@ -19,6 +28,7 @@ import org.springframework.web.bind.annotation.*;
  *
  * @author kawaiifox
  */
+@Tag(name = "Manage Project", description = "Query about uploaded project or delete them.")
 @RestController
 @RequestMapping("/api/project-info")
 public class ProjectInfoController {
@@ -49,6 +59,13 @@ public class ProjectInfoController {
      * @return {@link EntityModel<ProjectInfo>} with some useful links.
      * @throws ProjectInfoNotFoundException if {@link ProjectInfo} has not been found.
      */
+    @Operation(summary = "Query a project.", description = "Query an existing project by providing its id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = OK_200_RESPONSE),
+            @ApiResponse(responseCode = "404", description = "Project Info, " + NOT_FOUND_404,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+            )
+    })
     @GetMapping("/{projectId}")
     public EntityModel<ProjectInfo> one(@PathVariable("projectId") Long projectId) {
         return assembler.toModel(projectInfoRepository
@@ -65,6 +82,15 @@ public class ProjectInfoController {
      * @param projectId id of the {@link ProjectInfo} you want to delete.
      * @return Http no content response.
      */
+    @Operation(summary = "Delete a project.", description = "Delete an existing project by providing its id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = DELETE_SUCCESS_204,
+                    content = @Content(schema = @Schema)
+            ),
+            @ApiResponse(responseCode = "404", description = "Project Info, " + NOT_FOUND_404,
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+            )
+    })
     @DeleteMapping("/{projectId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Object delete(@PathVariable Long projectId) {
