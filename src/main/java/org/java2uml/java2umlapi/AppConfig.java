2b6ca235d4a62733cf3dcceb6ef9ca779b014ab8
kawaiifoxx <shreyanshg06@gmail.com>
2021-05-29 23:41:58 +0530

    Added a shutdown hook for ExecutorWrapper

--- src/main/java/org/java2uml/java2umlapi/AppConfig.java
+++ src/main/java/org/java2uml/java2umlapi/AppConfig.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.java2uml.java2umlapi.config.AppProperties;
+import org.java2uml.java2umlapi.executor.ExecutorWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,15 +14,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
+import javax.annotation.PreDestroy;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 @EnableJpaRepositories
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
 public class AppConfig {
+    private final Logger logger = LoggerFactory.getLogger(AppConfig.class);
     private final AppProperties appProperties;
+    private final ExecutorWrapper executorWrapper;
+    private final Long SHUTDOWN_TIME = 100L;
 
-    public AppConfig(AppProperties appProperties) {
+    public AppConfig(AppProperties appProperties, ExecutorWrapper executorWrapper) {
         this.appProperties = appProperties;
+        this.executorWrapper = executorWrapper;
     }
 
     @Bean
@@ -34,5 +45,22 @@ public class AppConfig {
                         .description("Java2UML API is simple web api which provides ability explore java source code.")
                         .license(new License().name("Apache 2.0")
                                 .url("https://raw.githubusercontent.com/kawaiifoxx/java2uml-api/main/LICENSE")));
+    }
+
+    /**
+     * Shutdown ExecutorWrapper before termination.
+     * @throws InterruptedException if some thread is interrupted in between then this exception is thrown.
+     */
+    @PreDestroy
+    public void onExitShutdownTaskExecutor() throws InterruptedException {
+        logger.info("Shutting Down ExecutorWrapper");
+        executorWrapper.shutdown();
+        if (!executorWrapper.awaitTermination(SHUTDOWN_TIME, TimeUnit.MILLISECONDS)) {
+            logger.warn("ExecutorWrapper did not terminate in specified time.");
+            List<Runnable> droppedTasks = executorWrapper.shutdownNow();
+            logger.info("Executor was terminated abruptly {} task did not finish", droppedTasks.size());
+        }
+
+        logger.info("Shutdown Successfully Finished.");
     }
 }
