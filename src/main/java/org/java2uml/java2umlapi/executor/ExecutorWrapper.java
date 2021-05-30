77e268641b873b35754673aa74921a748375141e
kawaiifoxx <shreyanshg06@gmail.com>
2021-05-30 13:26:37 +0530

    Added a new wrapper method called ExecutorWrapper.submit which takes in a callable.

--- src/main/java/org/java2uml/java2umlapi/executor/ExecutorWrapper.java
+++ src/main/java/org/java2uml/java2umlapi/executor/ExecutorWrapper.java
@@ -66,4 +66,31 @@ public class ExecutorWrapper {
     public Future<?> submit(Runnable task) throws RejectedExecutionException {
         return executorService.submit(task);
     }
+
+    /**
+     * Submits a value-returning task for execution and returns a
+     * Future representing the pending results of the task. The
+     * Future's {@code get} method will return the task's result upon
+     * successful completion.
+     *
+     * <p>
+     * If you would like to immediately block waiting
+     * for a task, you can use constructions of the form
+     * {@code result = exec.submit(aCallable).get();}
+     *
+     * <p>Note: The {@link Executors} class includes a set of methods
+     * that can convert some other common closure-like objects,
+     * for example, {@link java.security.PrivilegedAction} to
+     * {@link Callable} form so they can be submitted.
+     *
+     * @param task the task to submit
+     * @param <T> the type of the task's result
+     * @return a Future representing pending completion of the task
+     * @throws RejectedExecutionException if the task cannot be
+     *         scheduled for execution
+     * @throws NullPointerException if the task is null
+     */
+    public <T> Future<T> submit(Callable<T> task) {
+        return executorService.submit(task);
+    }
 }
