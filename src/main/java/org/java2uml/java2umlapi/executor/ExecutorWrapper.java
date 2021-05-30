58d95697f84232ddac9d52328d441925d80d16e0
kawaiifoxx <shreyanshg06@gmail.com>
2021-05-30 11:55:34 +0530

    Fixed java doc comment

--- src/main/java/org/java2uml/java2umlapi/executor/ExecutorWrapper.java
+++ src/main/java/org/java2uml/java2umlapi/executor/ExecutorWrapper.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.concurrent.*;
 
 /**
- * Task executor is a wrapper for the executor service.
+ * ExecutorWrapper is a wrapper for the executor service.
  *
  * @author kawaiifox
  */
@@ -31,10 +31,11 @@ public class ExecutorWrapper {
     /**
      * Blocks until all tasks have completed execution after a shutdown request, or the timeout occurs,
      * or the current thread is interrupted, whichever happens first.
+     *
      * @param timeout the maximum time to wait
-     * @param unit the time unit of the timeout argument
+     * @param unit    the time unit of the timeout argument
      * @return true if this executor terminated and false if the timeout elapsed before termination
-     * @throws InterruptedException  if interrupted while waiting
+     * @throws InterruptedException if interrupted while waiting
      */
     public boolean awaitTermination(Long timeout, TimeUnit unit) throws InterruptedException {
         return executorService.awaitTermination(timeout, unit);
@@ -59,8 +60,8 @@ public class ExecutorWrapper {
      * @param task the task to submit
      * @return a Future representing pending completion of the task
      * @throws RejectedExecutionException if the task cannot be
-     *         scheduled for execution
-     * @throws NullPointerException if the task is null
+     *                                    scheduled for execution
+     * @throws NullPointerException       if the task is null
      */
     public Future<?> submit(Runnable task) throws RejectedExecutionException {
         return executorService.submit(task);
