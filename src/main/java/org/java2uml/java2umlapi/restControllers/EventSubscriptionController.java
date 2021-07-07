ee33300201260f6e1641f280051f2f051dc5eb80
kawaiifoxx <shreyanshg06@gmail.com>
2021-07-07 22:11:54 +0530

    Added swagger docs.

--- src/main/java/org/java2uml/java2umlapi/restControllers/EventSubscriptionController.java
+++ src/main/java/org/java2uml/java2umlapi/restControllers/EventSubscriptionController.java
@@ -1,7 +1,14 @@
 package org.java2uml.java2umlapi.restControllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.java2uml.java2umlapi.fileStorage.repository.ProjectInfoRepository;
 import org.java2uml.java2umlapi.restControllers.exceptions.ProjectInfoNotFoundException;
+import org.java2uml.java2umlapi.restControllers.response.ErrorResponse;
 import org.java2uml.java2umlapi.restControllers.services.SSEEmitterCache;
 import org.java2uml.java2umlapi.restControllers.services.SSEEmitterCache.SSEventType;
 import org.slf4j.Logger;
@@ -16,6 +23,8 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 
+import static org.java2uml.java2umlapi.restControllers.SwaggerDescription.ERR_RESPONSE_MEDIA_TYPE;
+import static org.java2uml.java2umlapi.restControllers.SwaggerDescription.INTERNAL_SERVER_ERROR_DESC;
 import static org.java2uml.java2umlapi.restControllers.services.SSEEmitterCache.SSEventType.*;
 
 /**
@@ -24,6 +33,7 @@ import static org.java2uml.java2umlapi.restControllers.services.SSEEmitterCache.
  *
  * @author kawaiifoxx
  */
+@Tag(name = "Server Sent Events", description = "Subscribe to Events occurring on the server")
 @RestController
 @RequestMapping("/api/event")
 public class EventSubscriptionController {
@@ -39,21 +49,52 @@ public class EventSubscriptionController {
         this.emitterCache = emitterCache;
     }
 
+    @Operation(summary = "Subscribe to Parse Event", description = "A parse event is fired when a file is parsed.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Subscription Successful"),
+            @ApiResponse(responseCode = "500", description = INTERNAL_SERVER_ERROR_DESC,
+                    content = @Content(mediaType = ERR_RESPONSE_MEDIA_TYPE,
+                            schema = @Schema(implementation = ErrorResponse.class))),
+    })
     @GetMapping("/parse/{projectInfoId}")
     public SseEmitter subscribeToParseEvent(@PathVariable("projectInfoId") Long id) {
         return produceEmitter(id, PARSE);
     }
 
+    @Operation(summary = "Subscribe to Plant UML code Generation Event",
+            description = "A plant uml code generation event is fired when Plant UML code is generated.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Subscription Successful"),
+            @ApiResponse(responseCode = "500", description = INTERNAL_SERVER_ERROR_DESC,
+                    content = @Content(mediaType = ERR_RESPONSE_MEDIA_TYPE,
+                            schema = @Schema(implementation = ErrorResponse.class))),
+    })
     @GetMapping("/uml/code/{projectInfoId}")
     public SseEmitter subscribeToUMLCodeGenerationEvent(@PathVariable("projectInfoId") Long id) {
         return produceEmitter(id, UML_CODE_GENERATION);
     }
 
+    @Operation(summary = "Subscribe to UML Class Diagram Generation Event",
+            description = "A class diagram generation event is fired when a class diagram is generated.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Subscription Successful"),
+            @ApiResponse(responseCode = "500", description = INTERNAL_SERVER_ERROR_DESC,
+                    content = @Content(mediaType = ERR_RESPONSE_MEDIA_TYPE,
+                            schema = @Schema(implementation = ErrorResponse.class))),
+    })
     @GetMapping("/uml/svg/{projectInfoId}")
     public SseEmitter subscribeToUMLSVGGenerationEvent(@PathVariable("projectInfoId") Long id) {
         return produceEmitter(id, UML_SVG_GENERATION);
     }
 
+    @Operation(summary = "Subscribe to Project Model Generation Event",
+            description = "A Project Model generation event is fired when Project Model is generated.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Subscription Successful"),
+            @ApiResponse(responseCode = "500", description = INTERNAL_SERVER_ERROR_DESC,
+                    content = @Content(mediaType = ERR_RESPONSE_MEDIA_TYPE,
+                            schema = @Schema(implementation = ErrorResponse.class))),
+    })
     @GetMapping("/source/{projectInfoId}")
     public SseEmitter subscribeToSourceGeneration(@PathVariable("projectInfoId") Long id) {
         return produceEmitter(id, SOURCE_GENERATION);
@@ -63,7 +104,7 @@ public class EventSubscriptionController {
      * Produces {@link SseEmitter} with given configurations.
      * Performs all the necessary housekeeping.
      *
-     * @param id id of project.
+     * @param id        id of project.
      * @param eventType {@link SSEventType}
      * @return produced {@link SseEmitter}
      */
@@ -82,7 +123,7 @@ public class EventSubscriptionController {
      * @return {@link SseEmitter} with TIMEOUT set to 60s.
      */
     private SseEmitter getSseEmitter() {
-        var emitter = new SseEmitter( 60 * 1000L);
+        var emitter = new SseEmitter(60 * 1000L);
 
         try {
             emitter.send(SseEmitter.event().name("SUBSCRIBED"));
